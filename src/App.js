4851ee3965c965650a7bfda74c1f7811d48f4cea
allanstar-byte <allanmutisya263@gmail.com>
2024-07-22 11:12:22 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Log in</h3>
+      <input type="text" placeholder="Username" />
+      <br />
+      <input type="password" placeholder="Password" />
+      <br />
+      <button>Log in</button>
+      <hr />
+      <p>incase of anything contact support </p>
+    </div>
+  );
+}
+
+export default App;
