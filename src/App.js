024a836d43c2d8f56f007cbfeccbd794ff583f82
allanstar-byte <allanmutisya263@gmail.com>
2024-07-23 09:21:40 +0300

    custom rendering

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import './App.css';
 
 function App() {
+  const blogArr = [{
+    title: 'My title',
+    content: 'My content',},
+    {
+      title: 'My title 2',
+      content: 'My content 2',
+    },
+    {
+      title: 'My title 3',
+      content: 'My content 3',
+    }
+  ]
+  const blogcards = blogArr.map((item, pos)=>{
+    console.log(item.title)
+    return(
+      <div className='card' key={pos}>
+        <h1>{item.title}</h1>
+        <p>{item.content}</p>
+      </div>
+    )
+    
+  })
+  
   return (
     <div className="App">
-      <h3>Log in</h3>
-      <input type="text" placeholder="Username" />
-      <br />
-      <input type="password" placeholder="Password" />
-      <br />
-      <button>Log in</button>
-      <hr />
-      <p>incase of anything contact support </p>
+      {/* <h3>Log in</h3> */}
+      {/* <input type="text" placeholder="Username" /> */}
+      {/* <br /> */}
+      {/* <input type="password" placeholder="Password" /> */}
+      {/* <br /> */}
+      {/* <button>Log in</button> */}
+      {blogcards}
     </div>
   );
 }
